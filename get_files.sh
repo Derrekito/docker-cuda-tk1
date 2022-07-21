2662902ec00a5a72c20797482822393d73ed1d70
Derrek Landauer <Derrekito@users.noreply.github.com>
2022-07-21 10:14:33 -0600

    fixed typo in get_files.sh

--- get_files.sh
+++ get_files.sh
@@ -2,6 +2,6 @@
 
 #https://developer.nvidia.com/cuda-toolkit-65
 
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_6.5-14_amd64.deb
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_6.5-19_amd64.deb
 #wget http://developer.download.nvidia.com/compute/cuda/6_5/rel/installers/cuda_6.5.14_linux_64.run
 #wget http://developer.download.nvidia.com/compute/cuda/6_5/rel/installers/cuda_6.5.14_linux_32.run
